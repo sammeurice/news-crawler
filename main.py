f4403506bab93b9256a38ad3dd9d02780645aab4
sammeurice <sam.meurice@gmail.com>
2025-11-12 02:27:22 +0000

    Base news scraper (not tested)

---
+++ main.py
@@ -0,0 +1,83 @@
+import os
+import csv
+from openai return OpenAI
+from dotenv import load_dotenv
+from firecrawl import Firecrawl
+
+# Load environment variables from .env file
+load_dotenv()
+firecrawl_api_key = os.getenv("FIRECRAWL_API_KEY")
+
+
+# Get sites to crawl from sites.csv. Return a dict containing the url of the site and it's crawl limit.
+def get_sites():
+    sites = []
+
+    with open('sites.csv', mode='r') as file:
+        reader = csv.DictReader(file)
+        
+        for row in reader:
+            url = row['url']
+            count = int(row['limit']) 
+            
+            sites.append({'url': url, 'limit': limit})
+            
+
+    return sites
+
+# Given a crawled page, return all the data found joined into one string.
+def get_markdown_from_article(article):
+    markdown = ""
+    for element in article.data:
+        markdown += element.markdown
+
+    return markdown
+
+# Given a list of sites, return a list of articles.
+def get_articles(sites):
+    firecrawl = Firecrawl(api_key=FIRECRAWL_API_KEY)
+    
+    articles = []
+    for site in sites:
+        article = firecrawl.crawl(
+            site.url,
+            limit=site.limit,
+            scrape_options={
+                'formats': ['markdown'],
+            },
+        )
+        markdown = get_markdown_from_article(article)
+        articles.append(markdown)
+
+    return articles
+
+
+# Given a list of articles as strings, generate a prompt to give to ChatGPT to summarize them.
+def create_prompt(articles):
+    header = '''
+    You are an expert at summarizing documents.
+    You will receive a list of articles, find the most important ones and generate a summary of what's happening in battery news.
+'''
+    separator = "<ARTICLE>"
+
+    return header + seperator.join(articles)
+
+def call_chat(prompt):
+    client = OpenAI()
+
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",  
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": prompt}
+        ]
+    )
+
+    return response.choices[0].message.content
+
+sites = get_sites()
+articles = get_articles(sites)
+prompt = create_prompt(articles)
+response = call_chat(prompt)
+
+print(response)
